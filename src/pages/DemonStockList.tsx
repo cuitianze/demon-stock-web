916d47eea10b446438aed2fc2133ee866d2dcfa5
cuitianze <cuitianze123@gmail.com>
2024-01-31 00:17:40 +0800

    feat: 优化交互样式

--- src/pages/DemonStockList.tsx
+++ src/pages/DemonStockList.tsx
@@ -35,6 +35,9 @@ function DemonStockList() {
   const [stateCompareStockCodeList, setStateCompareStockCodeList] = useState<
     string[]
   >([]);
+  const [stateCompareStockNameList, setStateCompareStockNameList] = useState<
+    string[]
+  >([]);
 
   const onDateChange: DatePickerProps['onChange'] = (date, dateString) => {
     setStateDate(dateString);
@@ -127,14 +130,23 @@ function DemonStockList() {
 
   // 对比数据
   useEffect(() => {
-    if (!stateDataList.length || !stateCompareDataList.length) {
+    if (
+      !stateDataList.length ||
+      !stateCompareDataList.length ||
+      JSON.stringify(stateDataList) === JSON.stringify(stateCompareDataList)
+    ) {
       setStateCompareStockCodeList([]);
+      setStateCompareStockNameList([]);
       return;
     }
-    const compareStockCodeList = stateCompareDataList.map(compareDataItem => {
-      return compareDataItem['股票代码'];
+    const compareStockCodeList: string[] = [];
+    const compareStockNameList: string[] = [];
+    stateCompareDataList.forEach(compareDataItem => {
+      compareStockCodeList.push(compareDataItem['股票代码']);
+      compareStockNameList.push(compareDataItem['股票名称']);
     });
     setStateCompareStockCodeList(compareStockCodeList);
+    setStateCompareStockNameList(compareStockNameList);
   }, [stateDataList, stateCompareDataList]);
 
   const numberOptions = [
@@ -149,33 +161,50 @@ function DemonStockList() {
   const s2Options = {
     width: 2500,
     height: 1200,
+    showTooltip: true,
     interaction: {
       linkFields: ['股票代码'],
+      hoverHighlight: true,
     },
     conditions: {
+      text: [
+        {
+          field: '股票名称',
+          mapping: (value: any, record: any) => {
+            if (!stateCompareDataList.length) return;
+            if (!record || !record.query) return;
+            if (!stateCompareStockNameList.length) return;
+            if (stateCompareStockNameList.includes(record.query['股票名称'])) {
+              return {
+                fill: 'red',
+              };
+            } else {
+              return {
+                fill: 'green',
+              };
+            }
+          },
+        },
+      ],
       background: [
         {
           field: new RegExp(
-            '涨停原因|涨停原因个股涨停个数|股票名称|股票代码|板块|连板数|涨停时间_D|融资融券|回封|振幅|连板标签|封单_D|最大封单_D|主力净额_D|主力买入_D|主力卖出|主力卖出_D|成交额_D|实际流通_D|实际换手',
+            '股票名称|股票代码|板块|连板数|涨停时间_D|融资融券|回封|振幅|连板标签|封单_D|最大封单_D|主力净额_D|主力买入_D|主力卖出|主力卖出_D|成交额_D|实际流通_D|实际换手',
           ),
           mapping(value: any, record: any) {
             if (!record) return;
-            if (
-              record.query &&
-              record.query['股票代码'] &&
-              stateCompareStockCodeList.length
-            ) {
-              if (
-                !stateCompareStockCodeList.includes(record.query['股票代码'])
-              ) {
+            if (record['股票代码'] && stateCompareStockCodeList.length) {
+              if (!stateCompareStockCodeList.includes(record['股票代码'])) {
                 return {
                   fill: '#e3f1e5',
                 };
-              } 
+              }
             }
-            if (record.query && record.query['股票代码']) {
-              console.log(record, record.query['股票代码'], record.query['回封'], record.query['主力卖出'])
-              if (record.query['回封'] == 1 || Number(record.query['主力卖出']) < -50000000) {
+            if (record && record['股票代码']) {
+              if (
+                Number(record['回封']) === 1 ||
+                Number(record['主力卖出']) < -50000000
+              ) {
                 return {
                   fill: '#eefbbe',
                 };
@@ -195,80 +224,52 @@ function DemonStockList() {
 
   const s2DataConfig = {
     fields: {
-      rows: [
-        '涨停原因',
-        '涨停原因个股涨停个数',
-        '股票名称',
+      rows: ['涨停原因', '涨停原因个股涨停个数', '股票名称'],
+      // columns: ['type'],
+      values: [
         '股票代码',
+        '涨停时间_D',
         '板块',
         '连板数',
-        '涨停时间_D',
+        '连板标签',
         '融资融券',
         '回封',
-        '主力卖出',
+        '主力卖出_D',
+        '主力买入_D',
         '振幅',
-        '连板标签',
         '封单_D',
         '最大封单_D',
         '主力净额_D',
-        '主力买入_D',
-        '主力卖出_D',
         '成交额_D',
         '实际流通_D',
         '实际换手',
       ],
-      //   columns: ['type'],
-      values: ['主力卖出'],
     },
     meta: [
-      {
-        field: '涨停原因',
-        name: '涨停原因',
-      },
       {
         field: '涨停原因个股涨停个数',
         name: '涨停数',
       },
       {
-        field: '股票代码',
-        name: '股票代码',
-      },
-      {
-        field: '板块',
-        name: '板块',
-      },
-      {
-        field: '连板数',
-        name: '连板数',
-      },
-      {
         field: '融资融券',
         name: '融资融券',
         formatter: (value: any) => {
-          return value === '1' ? '是' : '';
+          return value === 1 ? '是' : '';
         },
       },
       {
         field: '涨停时间_D',
         name: '涨停时间',
         formatter: (value: any) => {
-          return value.split(' ')[1];
+          return value && value.split(' ')[1];
         },
       },
       {
         field: '回封',
         name: '回封',
         formatter: (value: any) => {
-          return value === '1' ? '是' : '';
+          return value === 1 ? '是' : '';
         },
-      },
-      {
-        field: '振幅',
-        name: '振幅',
-      },
-      {
-        field: '连板标签',
-        name: '连板标签',
       },
       {
         field: '封单_D',
@@ -302,7 +303,7 @@ function DemonStockList() {
         field: '实际换手',
         name: '实际换手',
         formatter: (value: any) => {
-          return `${value}%`;
+          return value ? `${value}%` : '';
         },
       },
     ],
@@ -410,6 +411,36 @@ function DemonStockList() {
                 },
                 bolderText: {
                   textAlign: 'right',
+                },
+              },
+              // 数值单元格 (其他单元格同理)
+              dataCell: {
+                cell: {
+                  interactionState: {
+                    // 悬停聚焦: 关闭悬停单元格时出现的 "黑色边框"
+                    hoverFocus: {
+                      borderColor: 'transparent',
+                    },
+                    // 十字悬停
+                    hover: {
+                      backgroundColor: 'orange',
+                      backgroundOpacity: 0.5,
+                      borderColor: 'transparent',
+                      borderOpacity: 1,
+                    },
+                    // 选中背景色/边框
+                    selected: {
+                      backgroundColor: '#ffb764',
+                      borderColor: '#000',
+                      borderWidth: 1,
+                    },
+                    // 未选中背景色/边框
+                    unselected: {
+                      backgroundOpacity: 0.5,
+                      textOpacity: 0.1,
+                      opacity: 0,
+                    },
+                  },
                 },
               },
             },
