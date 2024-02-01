a6faa4265240401d8596211fc18b7868e241a339
cuitianze <cuitianze123@gmail.com>
2024-02-02 02:14:50 +0800

    chore: 本地直接代理线上数据

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,19 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://nas8.site:3888',
+      changeOrigin: true,
+      secure: false,
+    }),
+  );
+  app.use(
     '/public',
     createProxyMiddleware({
-      target: 'http://localhost:7001',
+      target: 'https://nas8.site:3888',
       changeOrigin: true,
+      secure: false,
     }),
   );
 };
